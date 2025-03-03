0f0938c0bf6e0124b3bce2fc33c2eec69844a8f3
badrive <badirov.faras@gmail.com>
2025-03-03 17:14:21 +0000

    test

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: bfaras <bfaras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 17:13:03 by bfaras            #+#    #+#             */
-/*   Updated: 2025/03/02 23:39:19 by bfaras           ###   ########.fr       */
+/*   Updated: 2025/03/03 17:13:32 by bfaras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,12 @@ int main ()
     char    *line;
     int     i;
     int     line_count;
-    
+    int     j;
+
     void *mlx_ptr;
     void *win_ptr;
     void *img_ptr;
     int width, height;
-
-    mlx_ptr = mlx_init();
-
-    win_ptr = mlx_new_window(mlx_ptr, 800, 600, "Eid mobark");
-    img_ptr = mlx_xpm_file_to_image(mlx_ptr, "eid_fitr.xpm", &width, &height);
-    mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
-    mlx_key_hook(win_ptr, key_handler, NULL);
-
-    // mlx_key_hook(win_ptr, key_handler, NULL);
-	mlx_loop(mlx_ptr);
 
     fd = open("maps/map.ber", O_RDONLY);
     map = NULL;
@@ -70,7 +61,54 @@ int main ()
         i++;
     }
     map[i] = NULL;
+    // checker
     ft_check(map,line_count);
+    //
+    // create the map
+    int len  = ft_strlen(map[0]) - 1;
+    mlx_ptr = mlx_init();
+    i = 0;
+    j = 0;
+    win_ptr = mlx_new_window(mlx_ptr, 64 * len, line_count * 64, "so_long");
+
+    while (i < line_count)
+    {
+        j = 0;
+        while (j < len)
+        {
+            if (map[i][j] == '1')
+            {
+                img_ptr = mlx_xpm_file_to_image(mlx_ptr, "textures/stone.xpm", &width, &height);
+                mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, j * 64, i * 64);           
+            }
+            else if (map[i][j] == '0')
+            {
+                img_ptr = mlx_xpm_file_to_image(mlx_ptr, "textures/dirty.xpm", &width, &height);
+                mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, j * 64, i * 64);           
+            }
+            else if (map[i][j] == 'P')
+            {
+                img_ptr = mlx_xpm_file_to_image(mlx_ptr, "textures/sonic.xpm", &width, &height);
+                mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, j * 64, i * 64);           
+            }
+            else if (map[i][j] == 'C')
+            {
+                img_ptr = mlx_xpm_file_to_image(mlx_ptr, "textures/coin.xpm", &width, &height);
+                mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, j * 64, i * 64);           
+            }
+            else if (map[i][j] == 'E')
+            {
+                img_ptr = mlx_xpm_file_to_image(mlx_ptr, "textures/exit.xpm", &width, &height);
+                mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, j * 64, i * 64);           
+            }
+            j++;
+        }
+        i++;
+    }
+
+    mlx_key_hook(win_ptr, key_handler, NULL);
+	mlx_loop(mlx_ptr);
+    //
     close(fd);
 
     i = 0;
